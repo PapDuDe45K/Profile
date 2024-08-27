2335fabfa169c0497032d774e4423258ce4590ce
Prior_Kiing <105079499+PapDuDe45K@users.noreply.github.com>
2024-08-28 00:46:12 +0200

    Profiler Commit

--- myprofile/myprofile/wsgi.py
+++ myprofile/myprofile/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myprofile.settings')
 
 application = get_wsgi_application()
+
+app = application#for conneting to vercel vercel.app
