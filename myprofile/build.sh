fa65c4f7f2c644dd942452ba38d04fff26bd035b
Prior_Kiing <105079499+PapDuDe45K@users.noreply.github.com>
2024-08-29 23:33:36 +0200

    Security updated yaml

--- myprofile/build.sh
+++ myprofile/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -0 errexit
-#python manage.py migrate
-
 pip install -r requirements.txt
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
 python manage.py migrate
