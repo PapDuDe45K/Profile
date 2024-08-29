5589dff64fbc3f6892282a5047d2870f9c106470
Prior_Kiing <105079499+PapDuDe45K@users.noreply.github.com>
2024-08-29 21:35:50 +0200

    waitress

--- waitress_server.py
+++ waitress_server.py
@@ -2,4 +2,5 @@ from waitress import serve
 from myprofile.wsgi import application
 
 if __name__ == "__main__":
+    print("Starting Waitress server...")
     serve(application, host='0.0.0.0', port=8080)
