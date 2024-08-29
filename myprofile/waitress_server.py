b198205ef81d91209fb385f07c72390fab3d493f
Prior_Kiing <105079499+PapDuDe45K@users.noreply.github.com>
2024-08-29 21:39:18 +0200

    wsgi update

--- myprofile/waitress_server.py
+++ myprofile/waitress_server.py
@@ -1,6 +1,5 @@
 from waitress import serve
 from myprofile.wsgi import application
 
-if __name__ == "__main__":
-    print("Starting Waitress server...")
-    serve(application, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    serve(application, host='0.0.0.0', port=8000)
